35e43258f8915efa887c9f2bc3b68e0dc48281a2
l955a6 <75358673+l955a6@users.noreply.github.com>
2026-06-07 12:24:12 +0900

    ダミーのentrypointの作成

---
+++ suuji-1024-incrementation-monitor/src/main/scala/Suuji1024IncrementationMonitor.scala
@@ -0,0 +1,8 @@
+package object Suuji1024IncrementationMonitor {
+  def main(args: Array[String]): Unit =
+    println(
+      s"""あずきインターネットにおける、正式な表記方法は下記となります。
+         |- suuji_1024
+         |- すうじ１０２４""".stripMargin
+    )
+}
