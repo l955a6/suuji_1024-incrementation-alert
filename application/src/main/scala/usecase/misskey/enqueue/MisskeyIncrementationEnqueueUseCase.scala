8177f260383745d86c33ca3e868051ebeee90a38
l955a6 <75358673+l955a6@users.noreply.github.com>
2026-06-21 22:33:24 +0900

    usecaseでLoggerを使えるようにした

--- application/src/main/scala/usecase/misskey/enqueue/MisskeyIncrementationEnqueueUseCase.scala
+++ application/src/main/scala/usecase/misskey/enqueue/MisskeyIncrementationEnqueueUseCase.scala
@@ -2,10 +2,11 @@ package blue.l955a6.incrementationMonitor.application.usecase.misskey.enqueue
 
 import blue.l955a6.incrementationMonitor.application.integration.MessageReader
 import cats.effect.kernel.Async
+import org.typelevel.log4cats.Logger
 
 class MisskeyIncrementationEnqueueUseCase(
   reader: MessageReader
 ) {
-  def run[F[_]: Async](): F[Unit] =
+  def run[F[_]: Async: Logger](): F[Unit] =
     reader.connect()
 }
--- entrypoint/src/main/scala/Suuji1024IncrementationMonitor.scala
+++ entrypoint/src/main/scala/Suuji1024IncrementationMonitor.scala
@@ -1,13 +1,16 @@
 package blue.l955a6.incrementationMonitor.entrypoint
 
-import blue.l955a6.incrementationMonitor.di.MisskeyMessageReaderDesign
 import blue.l955a6.incrementationMonitor.application.usecase.misskey.enqueue.MisskeyIncrementationEnqueueUseCase
+import blue.l955a6.incrementationMonitor.di.MisskeyMessageReaderDesign
 import cats.effect.IO
 import cats.effect.kernel.Async
 import cats.effect.unsafe.implicits.global
+import org.typelevel.log4cats.Logger
+import org.typelevel.log4cats.slf4j.Slf4jLogger
 
 object Suuji1024IncrementationMonitor {
   given Async[IO] = IO.asyncForIO
+  given Logger[IO] = Slf4jLogger.getLogger[IO]
 
   def main(args: Array[String]): Unit =
     MisskeyMessageReaderDesign.design.build[MisskeyIncrementationEnqueueUseCase] { app =>
